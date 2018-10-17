225c3738e4cfc8a9a6f971f8d59a1fbaace50f8a
TabCen <964267617@qq.com>
2018-10-17 11:03:04 +0800

    添加header

---
+++ CFNetworkingHelper/Classes/CFNetworkingHelper.h
@@ -0,0 +1,24 @@
+//
+//  CFNetworkingHelper.h
+//  Pods
+//
+//  Created by David on 2018/10/17.
+//
+
+#import <Foundation/Foundation.h>
+
+
+#ifndef _CFNETWORKINGHELPER_
+    #define _CFNETWORKINGHELPER_
+
+#if __has_include(<CFNetworkingHelper/CFNetworkingHelper.h>)
+
+#import <CFNetworkingHelper/CFNetworking.h>
+
+#else
+
+#import "CFNetworking.h"
+
+#endif /* __has_include */
+
+#endif /* CFNetworkingHelper_h */
